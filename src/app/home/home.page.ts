1bcf9b73f08d6118a9cf201ce248f55eefde0e39
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-03-10 15:31:56 +0800

    Add time algorithm

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as dateFns from 'date-fns';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  timeOfDay: Date = new Date();
+  timeString: string = '12:00';
 
   constructor() {}
+
+  ngOnInit() {
+    this.timeString = dateFns.format(this.timeOfDay, 'h:mm A');
+  }
 
 }
