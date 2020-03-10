4f47c175e83dc431abba996a505557b9e1b201c9
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-03-10 16:31:28 +0800

    Add animations codes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer } from '@angular/core';
 import * as dateFns from 'date-fns';
 
 @Component({
@@ -11,10 +11,73 @@ export class HomePage implements OnInit {
   timeOfDay: Date = new Date();
   timeString: string = '12:00';
 
-  constructor() {}
+  sky: any;
+  entireSun: any;
+  allClouds: any;
+  clouds: any;
+
+  constructor(private renderer: Renderer) {}
 
   ngOnInit() {
     this.timeString = dateFns.format(this.timeOfDay, 'h:mm a');
+
+    this.sky = document.querySelector('linearGradient [offset="1"]');
+    this.entireSun = document.querySelector('#Sun');
+    this.clouds = document.querySelector('#Clouds path');
+
+    this.setTransitions();
+  }
+
+  setTransitions() {
+    this.renderer.setElementStyle(this.sky, 'transition', '1s linear');
+    this.renderer.setElementStyle(this.entireSun, 'transition', '1s linear');
+    this.clouds.forEach((cloud) => {
+      this.renderer.setElementStyle(cloud, 'transition', '1s linear');
+    });
+  }
+
+  setNight() {
+    this.renderer.setElementAttribute(this.sky, 'stop-color', '#141944');
+    this.renderer.setElementAttribute(this.entireSun, 'transform', 'translate(1,2000)');
+    this.renderer.setElementAttribute(this.entireSun, 'opacity', '1');
+
+    this.clouds.foreach((cloud) => {
+      this.renderer.setElementStyle(cloud, 'fill', '#fff');
+      this.renderer.setElementStyle(cloud, 'opacity', '0.2');
+    });
+  }
+
+  setDay() {
+    this.renderer.setElementAttribute(this.sky, 'stop-color', '#50a7dd');
+    this.renderer.setElementAttribute(this.entireSun, 'opacity', '1');
+    this.renderer.setElementAttribute(this.entireSun, 'transform', 'translate(1,1)');
+    
+    this.clouds.foreach((cloud) => {
+      this.renderer.setElementStyle(cloud, 'fill', '#fff');
+      this.renderer.setElementStyle(cloud, 'opacity', '1');
+    });
+  }
+
+  setSunset() {
+    this.renderer.setElementAttribute(this.entireSun, 'transform', 'translate(1, 1000)');
+    this.renderer.setElementAttribute(this.entireSun, 'opacity', '1');
+    this.renderer.setElementAttribute(this.sky, 'stop-color', '#e2905a');
+    
+    this.clouds.foreach((cloud) => {
+      this.renderer.setElementStyle(cloud, 'fill', '#e2c1d8');
+      this.renderer.setElementStyle(cloud, 'opacity', '0.4');
+    });
+  }
+
+  setCloudy() {
+    this.renderer.setElementAttribute(this.sky, 'stop-color', '#cecece');
+    this.renderer.setElementAttribute(this.entireSun, 'transform', 'translate(1,1)');
+    this.renderer.setElementAttribute(this.entireSun, 'opacity', '0.2');
+    
+    this.clouds.foreach((cloud) => {
+      this.renderer.setElementStyle(cloud, 'fill', '#fff');
+      this.renderer.setElementStyle(cloud, 'opacity', '1');
+    });
   }
 
 }
