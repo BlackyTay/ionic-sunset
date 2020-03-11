1c3605bac60f6509855554dd1501613976d38524
Tay Zheng Qiang <zhengqiang@goodsane.com>
2020-03-11 10:58:44 +0800

    - Fix height for svg - Add screen orientation plugin - Fix screen orientation to portrait

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Renderer } from '@angular/core';
 import * as dateFns from 'date-fns';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx'
 
 @Component({
   selector: 'app-home',
@@ -16,14 +17,15 @@ export class HomePage implements OnInit {
   allClouds: any;
   clouds: any;
 
-  constructor(private renderer: Renderer) {}
+  constructor(private renderer: Renderer, private screenOrientation: ScreenOrientation) {}
 
   ngOnInit() {
+    this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT);
     this.timeString = dateFns.format(this.timeOfDay, 'h:mm a');
 
     this.sky = document.querySelector('linearGradient [offset="1"]');
     this.entireSun = document.querySelector('#Sun');
-    this.clouds = document.querySelector('#Clouds path');
+    this.clouds = document.querySelectorAll('#Clouds path');
 
     this.setTransitions();
   }
@@ -31,7 +33,7 @@ export class HomePage implements OnInit {
   setTransitions() {
     this.renderer.setElementStyle(this.sky, 'transition', '1s linear');
     this.renderer.setElementStyle(this.entireSun, 'transition', '1s linear');
-    this.clouds.forEach((cloud) => {
+    this.clouds.forEach(cloud => {
       this.renderer.setElementStyle(cloud, 'transition', '1s linear');
     });
   }
@@ -41,7 +43,7 @@ export class HomePage implements OnInit {
     this.renderer.setElementAttribute(this.entireSun, 'transform', 'translate(1,2000)');
     this.renderer.setElementAttribute(this.entireSun, 'opacity', '1');
 
-    this.clouds.foreach((cloud) => {
+    this.clouds.forEach(cloud => {
       this.renderer.setElementStyle(cloud, 'fill', '#fff');
       this.renderer.setElementStyle(cloud, 'opacity', '0.2');
     });
@@ -52,7 +54,7 @@ export class HomePage implements OnInit {
     this.renderer.setElementAttribute(this.entireSun, 'opacity', '1');
     this.renderer.setElementAttribute(this.entireSun, 'transform', 'translate(1,1)');
     
-    this.clouds.foreach((cloud) => {
+    this.clouds.forEach(cloud => {
       this.renderer.setElementStyle(cloud, 'fill', '#fff');
       this.renderer.setElementStyle(cloud, 'opacity', '1');
     });
@@ -62,8 +64,8 @@ export class HomePage implements OnInit {
     this.renderer.setElementAttribute(this.entireSun, 'transform', 'translate(1, 1000)');
     this.renderer.setElementAttribute(this.entireSun, 'opacity', '1');
     this.renderer.setElementAttribute(this.sky, 'stop-color', '#e2905a');
-    
-    this.clouds.foreach((cloud) => {
+
+    this.clouds.forEach(cloud => {
       this.renderer.setElementStyle(cloud, 'fill', '#e2c1d8');
       this.renderer.setElementStyle(cloud, 'opacity', '0.4');
     });
@@ -74,7 +76,7 @@ export class HomePage implements OnInit {
     this.renderer.setElementAttribute(this.entireSun, 'transform', 'translate(1,1)');
     this.renderer.setElementAttribute(this.entireSun, 'opacity', '0.2');
     
-    this.clouds.foreach((cloud) => {
+    this.clouds.forEach(cloud => {
       this.renderer.setElementStyle(cloud, 'fill', '#fff');
       this.renderer.setElementStyle(cloud, 'opacity', '1');
     });
